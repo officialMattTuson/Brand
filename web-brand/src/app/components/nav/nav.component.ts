1ddac9279f906917ab210ca39a692e985296d4a4
Matthew Tuson <mtuson1414@gmail.com>
2025-03-06 18:23:48 +1300

    add initial components and styles for home, about, and navigation

---
+++ web-brand/src/app/components/nav/nav.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  imports: [CommonModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss',
+})
+export class NavComponent {}
