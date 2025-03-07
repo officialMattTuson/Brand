1c5a9c11219df3badfeaedfb270b12f62d1e56e5
Matthew Tuson <mtuson1414@gmail.com>
2025-03-07 20:47:49 +1300

    refactor home component: simplify typewriter effect by removing cursor and unused imports

--- web-brand/src/app/components/home/home.component.ts
+++ web-brand/src/app/components/home/home.component.ts
@@ -1,11 +1,4 @@
-import {
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  Renderer2,
-  ViewChild,
-} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -15,37 +8,25 @@ import {
 })
 export class HomeComponent implements AfterViewInit {
   @ViewChild('typewriter', { static: false }) typewriter!: ElementRef;
-  @ViewChild('cursor', { static: false }) cursor!: ElementRef;
-  text = 'Hello, my name is'; // The text to be typed
-  speed = 150; // Typing speed in ms
+  text = 'Hello, my name is';
+  speed = 150;
 
   ngAfterViewInit(): void {
     this.startTypingEffect();
   }
 
   startTypingEffect() {
-    if (!this.typewriter || !this.cursor) {
-      console.error('Error: ViewChild not detected.');
-      return;
-    }
-
     const element = this.typewriter.nativeElement;
-    const cursor = this.cursor.nativeElement;
-    element.innerHTML = ''; // Ensure it's empty
+    element.innerHTML = '';
 
     let i = 0;
     const typingInterval = setInterval(() => {
       if (i < this.text.length) {
-        element.innerHTML += this.text.charAt(i); 
+        element.innerHTML += this.text.charAt(i);
 
-        // Move the cursor
-        cursor.style.transform = `translateX(${i * 8}px)`; 
         i++;
       } else {
         clearInterval(typingInterval);
-
-        cursor.style.animation = 'none';
-        cursor.style.opacity = '0';
       }
     }, this.speed);
   }
