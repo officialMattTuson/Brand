70771a8147913228894e756bc55ff19af8cc5463
Matthew Tuson <mtuson1414@gmail.com>
2025-06-02 19:55:50 +1200

    refactor navigation component: remove mouse tracking functionality, restructure HTML for better organization, and enhance SCSS styles for improved layout and aesthetics

--- web-brand/src/app/app.component.ts
+++ web-brand/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 
@@ -10,13 +10,4 @@ import { NavComponent } from './components/nav/nav.component';
 })
 export class AppComponent {
   title = 'web-brand';
-
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove(event: MouseEvent) {
-    const xPercent = (event.clientX / window.innerWidth) * 100;
-    const yPercent = (event.clientY / window.innerHeight) * 100;
-
-    document.body.style.setProperty('--mouse-x', `${xPercent}%`);
-    document.body.style.setProperty('--mouse-y', `${yPercent}%`);
-  }
 }
